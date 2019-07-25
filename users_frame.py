7ac87bab351c6a73a7f493b7f976352bdee27739
markbrody <mark.brody@gmail.com>
2019-07-25 12:25:09 -0500

    updated some formatting

--- users_frame.py
+++ users_frame.py
@@ -14,6 +14,7 @@ import io
 class UsersFrame(GuiFrame):
 
     IDLE_TIME = 119
+    THUMBNAIL_SIZE = 32
 
     def __init__(self, parent, controller, user):
         sys.path.append(os.getcwd())
@@ -23,44 +24,44 @@ class UsersFrame(GuiFrame):
         self.update_last_user_interaction()
 
         self.widgets = [{
-            "name":"header_frame",
-            "class":HeaderFrame, 
-            "init":{"master":None,"controller":self.controller,},
-            "grid":{"row":0,"sticky":"ew",}
+            "name": "header_frame",
+            "class": HeaderFrame, 
+            "init": {"master":None, "controller":self.controller, },
+            "grid": {"row":0, "sticky": "ew", }
         },{
-            "name":"body_frame",
-            "class":tk.Frame,
-            "init":{"master":None,"background":"#fcfcfa"},
-            "grid":{"row":1,"sticky":"ew"},
+            "name": "body_frame",
+            "class": tk.Frame,
+            "init": {"master":None, "background": "#fcfcfa"},
+            "grid": {"row":1, "sticky": "ew"},
         },{
-            "name":"title_frame",
-            "class":tk.Frame,
-            "init":{"master":"body_frame","background":"#fcfcfa"},
-            "grid":{"row":0,"columnspan":2,"sticky":"new","padx":40,},
+            "name": "title_frame",
+            "class": tk.Frame,
+            "init": {"master": "body_frame", "background": "#fcfcfa"},
+            "grid": {"row":0, "columnspan":2, "sticky": "new", "padx":40, },
         },{
-            "name":"title_label",
-            "class":tk.Label,
-            "init":{"master":"title_frame","text":"Users","background":"#fcfcfa",
-                    "anchor":"w","font":controller.title_font},
-            "grid":{"row":0,"sticky":"w","pady":10},
+            "name": "title_label",
+            "class": tk.Label,
+            "init": {"master": "title_frame", "text": "Users", "background": "#fcfcfa",
+                    "anchor": "w", "font":controller.title_font},
+            "grid": {"row":0, "sticky": "w", "pady":10},
         },{
-            "name":"filters_frame",
-            "class":tk.Frame,
-            "init":{"master":"title_frame","background":"#fcfcfa",},
-            "grid":{"row":0,"column":2,"sticky":"e",},
+            "name": "filters_frame",
+            "class": tk.Frame,
+            "init": {"master": "title_frame", "background": "#fcfcfa", },
+            "grid": {"row":0, "column":2, "sticky": "e", },
         },{
-            "name":"hr_frame",
-            "class":tk.Frame,
-            "init":{"master":"body_frame","width":900,"background":"#8ab365","height":4},
-            "grid":{"row":1,"columnspan":2,"sticky":"new","padx":40},
+            "name": "hr_frame",
+            "class": tk.Frame,
+            "init": {"master": "body_frame", "width":900, "background": "#8ab365", "height":4},
+            "grid": {"row":1, "columnspan":2, "sticky": "new", "padx":40},
         },{
-            "name":"treeview_frame",
-            "class":TreeviewFrame,
-            "init":{
+            "name": "treeview_frame",
+            "class": TreeviewFrame,
+            "init": {
                 "master": "body_frame",
                 "controller": self.controller,
                 "belongs_to": self,
-                "columns":{
+                "columns": {
                     "id": {"text": "ID", "width": 0, "exclude": True, },
                     "email": {"text": "Email", "width": 182, },
                     "name": {"text": "Name", "width": 253, },
@@ -68,80 +69,85 @@ class UsersFrame(GuiFrame):
                     "created": {"text": "Time", "width": 200, },
                 },
             },
-            "grid":{"row":2,"column":0,"sticky":"n","padx":(40,5),"pady":20,},
+            "grid": {"row":2, "column":0, "sticky": "n", "padx": (40,5), "pady":20, },
         },{
-            "name":"details_frame",
-            "class":tk.Frame,
-            "init":{"master":"body_frame","background":"#fcfcfa",},
-            "grid":{"row":2,"column":1,"sticky":"n","padx":(5,40),"pady":20,},
+            "name": "details_frame",
+            "class": tk.Frame,
+            "init": {"master": "body_frame", "background": "#fcfcfa", },
+            "grid": {"row":2, "column":1, "sticky": "n", "padx": (5,40), "pady":20, },
         },{
-            "name":"user_email_label",
-             "class":tk.Label,
-             "init":{"master":"details_frame","background":"#fcfcfa",
-                     "font":"Helvetica 12 bold","text":"Email:",},
-             "grid":{"row":0,"columnspan":2,"sticky":"nw","pady":(3,0),},
+            "name": "user_email_label",
+            "class": tk.Label,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                     "font": "Helvetica 12 bold", "text": "Email: ", },
+            "grid": {"row":0, "columnspan":2, "sticky": "nw", "pady": (3,0), },
          },{
-            "name":"user_email_text",
-            "class":tk.Text,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "highlightthickness":0,"width":50,"height":1,},
-            "grid":{"row":1,"columnspan":2,"sticky":"new","padx":(5,0),},
+            "name": "user_email_text",
+            "class": tk.Text,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "highlightthickness":0, "width":50, "height":1, },
+            "grid": {"row":1, "columnspan":2, "sticky": "new", "padx": (5,0), },
         },{
-            "name":"user_name_label",
-            "class":tk.Label,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "font":"Helvetica 12 bold","text":"Name:",},
-            "grid":{"row":2,"sticky":"nw","pady":(10,0),}
+            "name": "user_name_label",
+            "class": tk.Label,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "font": "Helvetica 12 bold", "text": "Name: ", },
+            "grid": {"row":2, "sticky": "nw", "pady": (10,0), }
         },{
-            "name":"user_name_text",
-            "class":tk.Text,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "highlightthickness":0,"width":24,"height":1,},
-            "grid":{"row":3,"sticky":"new","padx":(5,0),},
+            "name": "user_name_text",
+            "class": tk.Text,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "highlightthickness":0, "width":24, "height":1, },
+            "grid": {"row":3, "sticky": "new", "padx": (5,0), },
         },{
-            "name":"user_phone_label",
-            "class":tk.Label,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "font":"Helvetica 12 bold","text":"Phone:",},
-            "grid":{"row":2,"column":1,"sticky":"nw","pady":(10,0),}
+            "name": "user_phone_label",
+            "class": tk.Label,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "font": "Helvetica 12 bold", "text": "Phone: ", },
+            "grid": {"row":2, "column":1, "sticky": "nw", "pady": (10,0), }
         },{
-            "name":"user_phone_text",
-            "class":tk.Text,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "highlightthickness":0,"width":24,"height":1,},
-            "grid":{"row":3,"column":1,"sticky":"new","padx":(5,0),},
+            "name": "user_phone_text",
+            "class": tk.Text,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "highlightthickness":0, "width":24, "height":1, },
+            "grid": {"row":3, "column":1, "sticky": "new", "padx": (5,0), },
         },{
-            "name":"user_address_label",
-            "class":tk.Label,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "font":"Helvetica 12 bold","text":"Address:",},
-            "grid":{"row":4,"columnspan":2,"sticky":"nw","pady":(10,0),}
+            "name": "user_address_label",
+            "class": tk.Label,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "font": "Helvetica 12 bold", "text": "Address: ", },
+            "grid": {"row":4, "columnspan":2, "sticky": "nw", "pady": (10,0), }
         },{
-            "name":"user_address_text",
-            "class":tk.Text,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "highlightthickness":0,"width":50,"height":4,},
-            "grid":{"row":5,"columnspan":2,"sticky":"new","padx":(5,0),},
+            "name": "user_address_text",
+            "class": tk.Text,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "highlightthickness":0, "width":50, "height":4, },
+            "grid": {"row":5, "columnspan":2, "sticky": "new", "padx": (5,0), },
         },{
-            "name":"details_hr_1_frame",
-            "class":tk.Frame,
-            "init":{"master":"details_frame","background":"#d0cbc1","height":1,},
-            "grid":{"row":6,"columnspan":2,"sticky":"ew","pady":(10,10),}
+            "name": "details_hr_1_frame",
+            "class": tk.Frame,
+            "init": {"master": "details_frame", "background": "#d0cbc1", "height":1, },
+            "grid": {"row":6, "columnspan":2, "sticky": "ew", "pady": (10,10), }
         },{
-            "name":"pets_label",
-            "class":tk.Label,
-            "init":{"master":"details_frame","background":"#fcfcfa",
-                    "font":"Helvetica 12 bold","text":"Pets",},
-            "grid":{"row":7,"columnspan":2,"sticky":"nw",}
+            "name": "pets_label",
+            "class": tk.Label,
+            "init": {"master": "details_frame", "background": "#fcfcfa",
+                    "font": "Helvetica 12 bold", "text": "Pets", },
+            "grid": {"row":7, "columnspan":2, "sticky": "nw", }
         },{
-            "name":"pets_frame",
-            "class":tk.Frame,
-            "init":{"master":"details_frame","background":"#d0cbc1","height":48,},
-            "grid":{"row":8,"columnspan":2,"sticky":"ew","pady":(10,10),}
+            "name": "pets_frame",
+            "class": tk.Frame,
+            "init": {"master": "details_frame", "background": "#d0cbc1", "height":48, },
+            "grid": {"row":8, "columnspan":2, "sticky": "ew", "pady": (10,10), }
+        },{
+            "name": "details_hr_1_frame",
+            "class": tk.Frame,
+            "init": {"master": "details_frame", "background": "#d0cbc1", "height":1, },
+            "grid": {"row":9, "columnspan":2, "sticky": "ew", "pady": (10,10), }
         },{
             "name": "navigation_frame",
             "class": NavigationFrame, 
-            "init":{
+            "init": {
                 "master": None,
                 "controller": self.controller,
                 "buttons": {
@@ -178,12 +184,13 @@ class UsersFrame(GuiFrame):
             if "id" in result:
                 user_email = result['email']
                 user_name = result['name']
-                user_phone = result['phone']
+                user_phone = f"({result['phone'][:3]}) {result['phone'][3:6]}" \
+                             f"-{result['phone'][6:10]}" \
+                             if result['phone'] else ""
                 user_address = "\n".join((
                     result['name'],
-                    f"{result['address_1']}\n{result['address_2']}"
-                        if result['address_2']
-                        else result['address_1'],
+                    f"{result['address_1']}\n{result['address_2']}" \
+                        if result['address_2'] else result['address_1'],
                     f"{result['city']}, {result['state']} {result['zip']}"
                 ))
                 user_pets = result['pets']
@@ -223,33 +230,40 @@ class UsersFrame(GuiFrame):
         for key, value in details.items():
             text = getattr(self, key)
             text.insert(tk.END, value)
-        i = 0
+        for column in range(0, 4):
+            label = tk.Label(self.pets_frame, background="#fcfcfa", height=1)
+            label.grid(row=0, column=column, sticky="nsew")
+        row = 0
         for pet in pets:
-            self.print_pet(pet, i)
-            i += 1
+            self.print_pet(pet, row)
+            row += 1
 
-    def print_pet(self,pet, i):
+    def print_pet(self, pet, row):
         photo_image = ImageTk.PhotoImage(self.photo_image(pet['thumbnail_url']))
         image_label = tk.Label(self.pets_frame, background="#fcfcfa")
-        image_label.configure(image=photo_image, width=32, height=32)
-        image_label.grid(row=i, sticky="nsew", ipadx=5, ipady=2)
+        image_label.configure(image=photo_image, width=self.THUMBNAIL_SIZE,
+                              height=self.THUMBNAIL_SIZE)
+        image_label.grid(row=row, sticky="nsew", ipadx=5, ipady=2)
         image_label.image = photo_image
         name_label = tk.Label(self.pets_frame, text=pet['name'],
-                         background="#fcfcfa", anchor="w")
-        name_label.grid(row=i, column=1, sticky="nsew", ipadx=5)
+                              background="#fcfcfa", anchor="w")
+        name_label.grid(row=row, column=1, sticky="nsew", ipadx=5)
         pet_type_label = tk.Label(self.pets_frame, text=pet['pet_type']['name'],
-                         background="#fcfcfa")
-        pet_type_label.grid(row=i, column=2, sticky="nsew", ipadx=5)
+                                  background="#fcfcfa")
+        pet_type_label.grid(row=row, column=2, sticky="nsew", ipadx=5)
         tracking_id_label = tk.Label(self.pets_frame, text=pet['tracking_id'],
-                         background="#fcfcfa")
-        tracking_id_label.grid(row=i, column=3, sticky="nsew", ipadx=5)
+                                     background="#fcfcfa")
+        tracking_id_label.grid(row=row, column=3, sticky="nsew", ipadx=5)
 
     def photo_image(self, url):
         request = urllib.request.urlopen(url)
         data = request.read()
         request.close()
         image = Image.open(io.BytesIO(data))
-        return image.resize((32, 32), Image.ANTIALIAS)
+        return image.resize(
+            (self.THUMBNAIL_SIZE, self.THUMBNAIL_SIZE),
+            Image.ANTIALIAS
+        )
 
     def update_last_user_interaction(self):
         self.last_user_interaction = int(time.time())
